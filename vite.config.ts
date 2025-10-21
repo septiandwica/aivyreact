7d3f370051782610f4ef0e71d25a52b17dd49621
septiandwica <septiandwica03@gmail.com>
2025-10-21 08:11:21 +0700

    delete https localhost

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import fs from 'fs'
-import path from 'path'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'localhost-cert.pem')),
-    },
-    port: 5173,
-    host: true,
-  },
 })
